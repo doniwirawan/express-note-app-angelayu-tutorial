409ee10d571839fda4775f00d5b2d713000f1da6
doniwirawan <doniwirawan166@gmail.com>
2022-04-21 11:57:14 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.set('view engine','ejs')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+
+let items = [];
+
+
+app.get('/', (req,res) => {
+    const Today = new Date;
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    }
+
+    const day = Today.toLocaleDateString('id-ID', options)
+
+    res.render('list', {day, newListItem: items})
+})
+
+app.post('/', (req,res) => {
+    items.push(req.body.newItem);
+
+    res.redirect('/')
+
+})
+
+app.listen(3000, () => {
+    console.log('server running on port 3000')
+})
